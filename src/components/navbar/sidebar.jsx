717d6cc06380ff4f4eeaafc85f2bd0ac884fe190
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-25 02:15:43 +0800

    removed uncess changes

--- src/components/navbar/sidebar.jsx
+++ src/components/navbar/sidebar.jsx
@@ -3,7 +3,7 @@ import { AuthContext } from '../../context/AuthContext'
 import { useLocation, useNavigate } from 'react-router-dom'
 import {signOut} from "firebase/auth"
 import { auth } from '../../firebase'
-import { collection, query, where , getDocs } from 'firebase/firestore'
+import { collection, query, where , getDocs, onSnapshot } from 'firebase/firestore'
 import { db } from '../../firebase'
 import '../../newstyle.css'
 import { faChartSimple, faComments, faBullseye } from '@fortawesome/free-solid-svg-icons'
@@ -13,27 +13,10 @@ import SideBarItem from './sidebaritem'
 const Sidebar = ({menu}) => {
 
   const {currentUser} = useContext(AuthContext)
-  const [user, setUser] = useState([])
   const location = useLocation();
   const [active, setActive] = useState(1);
   const navigate = useNavigate()
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const querySnapshot = await getDocs(query(collection(db, 'user'), where('uid', '==', "fSLC9Wd2QxXnF6WwzWuzpHMa9233")));
-        querySnapshot.forEach((doc) => {
-          // console.log(doc.id, ' => ', doc.data());
-          setUser(doc.data())
-        });
-      } catch (error) {
-        console.error('Error getting user document: ', error);
-      }
-    };
-  
-    getUserData();
-  }, []);
-  
   const handleLogout = () => {
       signOut(auth).then(() => {
           navigate('/')
@@ -55,10 +38,8 @@ const Sidebar = ({menu}) => {
     return (
         <div className="sidebar">
           <div>
-            <img  src={user.photoUrl} alt="User Profile" />
-            <a className="username" href="/admin/profile">
-              {user.username}
-            </a>
+            <img  src={currentUser.photoURL} />
+            <a>{currentUser.displayName}</a>
             <ul>
               
             <h3>Menu</h3>
