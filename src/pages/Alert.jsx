9b934e82783c8cefcef337c6ab7b99066074a3c2
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-24 22:03:35 +0800

    Alert cahnges

--- src/pages/Alert.jsx
+++ src/pages/Alert.jsx
@@ -1,10 +1,13 @@
 import React , {useEffect , useState} from 'react';
 import '../style.css'
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar/sidebar';
 import { collection, query, where , getDocs } from 'firebase/firestore'
 import { db } from '../firebase'
 import '../style.css'
 import axios from 'axios';
+import Sidebar from '../components/navbar/sidebar';
+import sidebar_menu from '../components/navbar/sidebarmenu';
+import { SendNotif } from '../functions';
 
 const Alert = () => {
 
@@ -22,48 +25,9 @@ const Alert = () => {
     setMessage(event.target.value);
   };
 
-  const handleClick = async () => {
-    // Function to run when the button is clicked
-  
-    try {
-      const serverKey = 'AAAA6yw0V8Q:APA91bEtCvykmd26k3UjjNfS9KfMmSmIrS837HBG4FzLWwVL7rnYjKYrIqFTzohzAZnojib68PdS-GJOI6FGr-KspFhj12JhKSalabXI7JoZCee_I7B9Gv7bmwyuM6Ds4bq6lGbZw4on';
-      const fcmEndpoint = 'https://fcm.googleapis.com/fcm/send';
-  
-      const querySnapshot = await getDocs(collection(db, 'token'));
-      querySnapshot.docs.forEach(async (doc) => {
-        const tokens = querySnapshot.docs.map((doc) => doc.data());
-        console.log(tokens[0].token); // Logging the token for debugging purposes
-  
-        const dataArr = tokens[0].token;
-  
-        const messages = {
-          registration_ids: dataArr,
-          notification: {
-            title: title,
-            body: message,
-          },
-          data: {
-            // Optional data payload
-            // You can include custom data along with the notification
-            // e.g., a specific action to perform when the user taps the notification
-            // 'key': 'value'
-          },
-        };
-  
-        const response = await axios.post(fcmEndpoint, messages, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'key=' + serverKey,
-          },
-        });
-  
-        console.log('Notification sent successfully:', response.data);
-      });
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
+  const handleClick = () => {
+    SendNotif(title, message)
+  }
 
   useEffect(() => {
     const getUserData = async () => {
@@ -82,12 +46,9 @@ const Alert = () => {
   }, []);
  
   return (
-    <div>
-    {/* <Navbar/> */}
-    <div className="modal">
-      {/* <Navbar/> */}
-      {/* <div className="modal-container"> */}
-        <Navbar />
+    <div className='chatContainer'>
+    <div className="container">
+        <Sidebar menu={sidebar_menu} />
         <div className="modal-content">
           <h3>Alert</h3>
           <div className="content1">
