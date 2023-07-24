52f806cfd5141d2805735e122b18fa7c3edc980f
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-24 22:03:46 +0800

    moved sendnotif

---
+++ src/functions/index.jsx
@@ -0,0 +1,46 @@
+
+import { collection, query, where , getDocs } from 'firebase/firestore'
+import { db } from '../firebase'
+import axios from 'axios';
+
+export const SendNotif = async (title, message) => {
+    // Function to run when the button is clicked
+  
+    try {
+      const serverKey = 'AAAA6yw0V8Q:APA91bEtCvykmd26k3UjjNfS9KfMmSmIrS837HBG4FzLWwVL7rnYjKYrIqFTzohzAZnojib68PdS-GJOI6FGr-KspFhj12JhKSalabXI7JoZCee_I7B9Gv7bmwyuM6Ds4bq6lGbZw4on';
+      const fcmEndpoint = 'https://fcm.googleapis.com/fcm/send';
+  
+      const querySnapshot = await getDocs(collection(db, 'token'));
+      querySnapshot.docs.forEach(async (doc) => {
+        const tokens = querySnapshot.docs.map((doc) => doc.data());
+        console.log(tokens[0].token); // Logging the token for debugging purposes
+  
+        const dataArr = tokens[0].token;
+  
+        const messages = {
+          registration_ids: dataArr,
+          notification: {
+            title: title,
+            body: message,
+          },
+          data: {
+            // Optional data payload
+            // You can include custom data along with the notification
+            // e.g., a specific action to perform when the user taps the notification
+            // 'key': 'value'
+          },
+        };
+  
+        const response = await axios.post(fcmEndpoint, messages, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'key=' + serverKey,
+          },
+        });
+  
+        console.log('Notification sent successfully:', response.data);
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
