c31b379d2ce5d67c4a680df9df77843af925525b
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-26 20:42:57 +0800

    profile auth changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,21 @@
-import React , {useEffect , useState} from 'react';
-import '../style.css'
-import Navbar from './navbar/sidebar';
+import React , {useContext, useEffect , useState} from 'react';
 import { collection, query, where , getDocs } from 'firebase/firestore'
 import { db } from '../firebase'
-import '../style.css'
+import '../newstyle.css'
+import { AuthContext } from '../context/AuthContext';
+import Sidebar from './navbar/sidebar';
+import sidebar_menu from './navbar/sidebarmenu';
 
 const Profile = () => {
 
-  // const {currentUser} = useContext(AuthContext)
+  const {currentUser} = useContext(AuthContext)
   const [user, setUser] = useState([])
 
-  // const navigate = useNavigate()
-
+  
   useEffect(() => {
     const getUserData = async () => {
       try {
-        const querySnapshot = await getDocs(query(collection(db, 'user'), where('uid', '==', "fSLC9Wd2QxXnF6WwzWuzpHMa9233")));
+        const querySnapshot = await getDocs(query(collection(db, 'user'), where('uid', '==', currentUser?.uid)));
         querySnapshot.forEach((doc) => {
           setUser(doc.data())
         });
@@ -28,15 +28,15 @@ const Profile = () => {
   }, []);
  
   return (
-    <div className="modal">
-    <div className="modal-continer">
-     <Navbar/>
+    <div className="chatContainer">
+    <div className="container">
+     <Sidebar menu = {sidebar_menu} />
       <div className="modal-content">
         <h3>My Profile</h3>
       <div className="myProfilePic" >
       <img src={user.photoURL} style={{width: 100, height: 100}}/>
     </div>
-    <a href="/admin/profile">{user.name}</a>
+    <a href="/admin/profile">{user.username}</a>
       </div>
     </div>
     </div>
