86a0caaa771c20ac3d55a082640e460f2f34b6d5
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-30 22:26:35 +0800

    bootstrap and dependencies added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { ChatContextProvider } from "./context/ChatContext";
 import { Provider } from "react-redux";
 import {createStore} from 'redux'
 import rootReducer from "./redux/reducers/indexReducer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const store = createStore(
