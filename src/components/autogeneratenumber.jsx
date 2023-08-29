e03341e3ff084bea208b0f4c224f2b7f6805ba20
Roven Kyle Narra <ellamateogang@gmail.com>
2023-08-30 04:09:44 +0800

    limits to 25

--- src/components/autogeneratenumber.jsx
+++ src/components/autogeneratenumber.jsx
@@ -1,9 +1,9 @@
 const generateRandomString = () => {
     const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-%$!";
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let randomString = "";
   
-    for (let i = 0; i < 250; i++) {
+    for (let i = 0; i < 25; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
       randomString += characters[randomIndex];
     }
